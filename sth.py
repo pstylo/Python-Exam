f54b303550f114c85b450fef1f122886abaa26f1
pstylo <shabin.poon@gmail.com>
2024-06-17 14:42:30 +0200

    a

---
+++ sth.py
@@ -0,0 +1,3 @@
+"""Exercise: Quick Slicing
+From the following given string, slice every 7th element in reverse order by omitting all heading and tailing special characters with appropriate start and end positions.
+$#@|~aslbagjkels nspielbiack ipnzqnmyxrclwqpiofpgwirmvoiupq riepwqdjoi pioack o ocpalsig====%%%**>"""
