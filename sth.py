4fdff0f8a0511af14b9e6564e1c33564595b378d
pstylo <32639531+pstylo@users.noreply.github.com>
2024-06-18 18:53:45 +0200

    Update sth.py

--- sth.py
+++ sth.py
@@ -7,10 +7,5 @@ sting = "$#@|~aslbagjkels nspielbiack ipnzqnmyxrclwqpiofpgwirmvoiupq riepwqdjoi 
 print(sting[-11:4:-7])
 
 
-"""Exercise: Quick Slicing
-From the following given string, slice every 7th element in reverse order by omitting all heading and tailing special characters with appropriate start and end positions.
-$#@|~aslbagjkels nspielbiack ipnzqnmyxrclwqpiofpgwirmvoiupq riepwqdjoi pioack o ocpalsig====%%%**>"""
 
-sting = "$#@|~aslbagjkels nspielbiack ipnzqnmyxrclwqpiofpgwirmvoiupq riepwqdjoi pioack o ocpalsig====%%%**>"
-
-print(sting[-11:4:-7])
+test
