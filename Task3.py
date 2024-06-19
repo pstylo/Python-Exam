80d481aad2c308506548e3fa4b623d4bd854f619
pstylo <shabin.poon@gmail.com>
2024-06-19 15:55:38 +0200

    Update

--- Task3.py
+++ Task3.py
@@ -10,3 +10,19 @@ students = [
 s_students = sorted(students, key=lambda x: x['score'], reverse=True)
 
 print(1, s_students)
+
+#Task3b
+
+strings = ['data', 'science', 'python', 'comprehension', '3']
+
+char_frequency = {}
+
+for string in strings:
+    for char in string:
+        if char in char_frequency:
+            char_frequency[char] += 1
+        else:
+            char_frequency[char] = 1
+
+print(3, char_frequency)
+
---
+++ Task4.py
@@ -0,0 +1,6 @@
+#Task4a
+strings = ['data', 'science', 'python', 'comprehension', '2']
+
+unique_characters = {char for word in strings for char in word}
+
+print(2, unique_characters)
