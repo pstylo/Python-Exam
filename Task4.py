daf5c2f235add70e78f14a99514849399fe9881a
pstylo <shabin.poon@gmail.com>
2024-06-19 15:58:32 +0200

    Task4b

--- Task4.py
+++ Task4.py
@@ -4,3 +4,18 @@ strings = ['data', 'science', 'python', 'comprehension', '2']
 unique_characters = {char for word in strings for char in word}
 
 print(2, unique_characters)
+
+#Task4b
+
+strings = ['data', 'science', 'python', 'comprehension', '3']
+
+char_frequency = {}
+
+for string in strings:
+    for char in string:
+        if char in char_frequency:
+            char_frequency[char] += 1
+        else:
+            char_frequency[char] = 1
+
+print(3, char_frequency)
